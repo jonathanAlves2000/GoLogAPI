4b1f8f6a9ba8856103cab90d01fd0ae52b014234
Jonathan Alves <jonathanmatheusalves@gmail.com>
2026-03-01 18:08:43 -0300

    refactor: implementar lógica de auditoria

--- src/main/java/GoLogAPI/infra/jpa/JpaConfig.java
+++ src/main/java/GoLogAPI/infra/jpa/JpaConfig.java
@@ -1,9 +1,9 @@
-package GoLogAPI.infra.jpa;
+//package GoLogAPI.infra.jpa;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//
+//@Configuration
+//@EnableJpaAuditing
+//public class JpaConfig {
+//}
--- src/main/java/GoLogAPI/model/Audit.java
+++ src/main/java/GoLogAPI/model/Audit.java
@@ -1,26 +1,59 @@
 package GoLogAPI.model;
-
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.Instant;
+import java.util.UUID;
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class Audit {
+public abstract class Audit{
 
     @Column(name = "created_at", updatable = false)
-    @CreatedDate
     private Instant createdAt;
 
-    @Column(name = "update_at")
-    @LastModifiedDate
-    private Instant updateAt;
+    @Column(name = "created_by", updatable = false)
+    private UUID createdBy;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
 
     @Column(name = "active", nullable = false)
     private Boolean active = true;
+
+    @PrePersist
+    public void onPrePersist(){
+        createdAt = Instant.now();
+        createdBy = getUserAuthenticatedId();
+        updatedAt = this.createdAt;
+        updatedBy = getUserAuthenticatedName();
+    }
+
+    @PreUpdate
+    public void onPreUpdate(){
+        updatedAt = Instant.now();
+        updatedBy = getUserAuthenticatedName();
+    }
+
+    private UUID getUserAuthenticatedId(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth == null || !auth.isAuthenticated() || auth instanceof AnonymousAuthenticationToken){
+            return null;
+        }
+        User userLogin = (User) auth.getPrincipal();
+        return userLogin.getId();
+    }
+
+    private String getUserAuthenticatedName(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User userLogin = (User) auth.getPrincipal();
+        return (auth != null) ? userLogin.getName() : "System";
+    }
 }
